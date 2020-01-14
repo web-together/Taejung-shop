5e5e9502b2ca6f12aab7f00f65b69027111803ca
Minchul Kang <tegongkang@gmail.com>
2020-01-15 00:41:43 +0900

    rm later code

--- shop/views.py
+++ shop/views.py
@@ -17,11 +17,8 @@ def product_in_category(request, category_slug=None):
                   'categories': categories, 
                   'products': products})
 
-from cart.forms import AddProductForm
-
 # detail view
 def product_detail(request, id, product_slug=None):
     product = get_object_or_404(Product, id=id, slug=product_slug)
-    add_to_cart = AddProductForm(initial={'quantity':1})
 
     return render(request, 'shop/detail.html', {'product': product, 'add_to_cart':add_to_cart})
